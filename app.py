c5b9d964b82710ed75530c7e4c7e530ff22c3177
lvgalvaofilho <lvgalvaofilho@gmail.com>
2024-08-10 02:31:20 -0300

    update

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 
+"ola mundo!"
+
 chart_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=["lat", "lon"],
